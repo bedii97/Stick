1c00504e78180a5ccc0f9f1913d56d03d7b8e60a
bedii97 <bedii97@gmail.com>
2020-04-26 17:01:34 +0300

    Bottom Dialog

---
+++ app/src/main/java/com/example/stick/BottomDialog.java
@@ -0,0 +1,66 @@
+package com.example.stick;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomDialog extends BottomSheetDialogFragment{
+    public static final String TAG = "BottomDialog";
+    private BottomDialogListener mListener;
+
+    public interface BottomDialogListener{
+        void onAddItemClick(String text);
+    }
+
+    public static BottomDialog newInstance() {
+        return new BottomDialog();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_dialog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final EditText editText = view.findViewById(R.id.bottom_dialog_et);
+        Button button = view.findViewById(R.id.bottom_dialog_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onAddItemClick(editText.getText().toString());
+                dismiss();
+            }
+        });
+        //view.findViewById(R.id.bottom_dialog_et).setOnClickListener(this);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof BottomDialogListener) {
+            mListener = (BottomDialogListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement ItemClickListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
