acf5bc95bb9a622026bad916d0bd2b9e9f05360c
SerefCanOzer <sserefcanozer@gmail.com>
2020-04-29 11:20:59 +0300

    create note dialog redesign

--- app/src/main/java/com/example/stick/CreateNoteDialog.java
+++ app/src/main/java/com/example/stick/CreateNoteDialog.java
@@ -1,12 +1,8 @@
 package com.example.stick;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,32 +15,21 @@ public class CreateNoteDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        /*LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.dialog_create_note, null);*/
 
-        /*builder.setView(view)
-                .setTitle("Create Stick");
-
-        //define
-        return builder.create();*/
-
-        new MaterialAlertDialogBuilder(getActivity())
+        return new MaterialAlertDialogBuilder(getContext(), R.style.createNoteDialogTheme)
                 .setView(R.layout.dialog_create_note)
-                .setTitle("Sample")
-                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                .setPositiveButton(R.string.createNoteDialogPositiveButton, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
                     }
                 })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                .setNegativeButton(R.string.createNoteDialogNegativeButton, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
                     }
                 })
-                .show();
-        return builder.create();
+                .create();
     }
 }
